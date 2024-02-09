f978c65dbd813c02fb912100b28a996f59fb6c55
Dunn254 <124111950+Dunn254@users.noreply.github.com>
2024-02-08 16:38:10 -0800

    ideas from work thursday 2/8/24

---
+++ scripts.js
@@ -0,0 +1,6 @@
+//create board element(create a div element)
+const board = document.createElement("div");
+//set the ID
+board.id = "checkers-board";
+//add the class board
+board.classList.add("board")
