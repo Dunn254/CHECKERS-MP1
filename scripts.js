f6254b44a930bca71b438daf25b473db46d531a5
Dunn254 <124111950+Dunn254@users.noreply.github.com>
2024-02-09 19:30:05 -0800

    CSS wont create board so try JS

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
-//create board element(create a div element)
-const board = document.createElement("div");
-//set the ID
-board.id = "checkers-board";
-//add the class board
-board.classList.add("board")
+//create board class
+class board {
+    constructor() {
+        this.board = Array(8)
+        .fill(null)
+        .map(() => Array(8).fill(null));
+    }
+}
+
 //create squares using loop(not sure to use for or while...talk to Omar)
 for (let x = 1; x <=64; x++) { ... }
